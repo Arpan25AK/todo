cb4b128984cdcd7478afe6bee31d03675b3a6a73
ArpanAk <akarpan2005@gmail.com>
2025-12-30 19:05:43 +0530

    added class to each packages to call the dependecies

---
+++ src/main/java/com/example/todo/Entity/todo.java
@@ -0,0 +1,10 @@
+package com.example.todo.Entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+public class todo {
+
+}
