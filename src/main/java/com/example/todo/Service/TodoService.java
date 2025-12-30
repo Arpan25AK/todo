321e4b0f9f4892c87d4769a37bcdca660eee2616
ArpanAk <akarpan2005@gmail.com>
2025-12-30 19:45:49 +0530

    added the service block for save and other crud stuffs

--- src/main/java/com/example/todo/Service/TodoService.java
+++ src/main/java/com/example/todo/Service/TodoService.java
@@ -1,4 +1,31 @@
 package com.example.todo.Service;
 
+import com.example.todo.Entity.Todo;
+import com.example.todo.repository.TodoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
 public class TodoService {
+    private final TodoRepository todoRepository;
+
+    public List<Todo> getAllTodos(){
+        return todoRepository.findAll();
+    }
+
+    public Todo saveAllTodos(Todo todo){
+        return todoRepository.save(todo);
+    }
+
+    public Optional<Todo> getById(long id){
+        return todoRepository.findById(id);
+    }
+
+    public void deleteById(long id){
+         todoRepository.deleteById(id);
+    }
 }
